949b31f4270c73dc7dd73728237e9ec96798ccc7
mdyasarbe <mdyasarbe@gmail.com>
2021-10-25 23:16:45 +0530

    "To Main"

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MasterlayoutComponent } from './masterlayout/masterlayout.component';
+
+
+
+@NgModule({
+  declarations: [MasterlayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
